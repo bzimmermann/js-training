38a3a0bfe19f75e8b2b1913f4e887000b802a287
Bernd Zimmermann <bernd.zimmermann@gmail.com>
2013-12-23 14:30:07 +0100

    cleaned the code

--- weewikipaint/Jakefile.js
+++ weewikipaint/Jakefile.js
@@ -2,21 +2,41 @@
 
 "use strict";
 
-task("default",["lint"]);
+task("default", ["lint"]);
 
 desc("Lint everything");
 
-task("lint", [], function() {
-    var lint = require("./build/lint/lint_runner.js");
-
-    var files = new jake.FileList();
-    files.include("**/*.js");
-    files.exclude("node_modules");
-    files.exclude("_lint_runner_test.js");
-
+function nodeLintOptions() {
     var options = {
+        bitwise: true,
+        curly: false,
+        eqeqeq: true,
+        forin: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        noempty: true,
+        nonew: true,
+        regexp: true,
+        undef: true,
+        strict: true,
+        trailing: true,
         node: true
     };
+    return options;
+}
 
-    lint.validateFileList(files.toArray(), options, {});
+function jakeFileList() {
+    var flist = new jake.FileList()
+        .include("**/*.js")
+        .exclude("node_modules")
+        .exclude("_lint_runner_test.js")
+        .toArray();
+    return flist;
+}
+task("lint", [], function () {
+    var lint = require("./build/lint/lint_runner.js");
+    lint.validateFileList(jakeFileList(), nodeLintOptions(), {});
 });
+
